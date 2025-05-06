e55f1c2650e759d288e9f7251de60d45a5164310
arwijesinghe <arwijesinghe@gmail.com>
2025-05-06 13:44:00 +0530

    Update movies-list.jsx

--- components/home/movies-list.jsx
+++ components/home/movies-list.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Card } from "@/components/ui/card";
-import { MOVIES } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 import { getMovies } from "@/actions/movies";
@@ -16,7 +15,7 @@ export default async function MoviesList() {
     <div className="space-y-6">
       <div className="border-primary/20 bg-card shadow-xs rounded-lg border p-4">
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {MOVIES.map((movie) => {
+          {Movies.map((movie) => {
             return (
               <div key={movie.id} className="">
                 <Link href={`/movies/${movie.id}`}>
